aa8970c05dda780bd0624cdb55aa5116616f0802
NuradinAhmed <Nuradinahmed2012@gmail.com>
2021-08-22 22:06:57 +0300

    added navFavorite from home to work icons

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { 
+    FlatList,
+    StyleSheet, 
+    Text, 
+    TouchableOpacity,
+    View,
+ } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import {Icon} from "react-native-elements";
+
 
 const data = [
     {
@@ -15,21 +23,38 @@ const data = [
         location: "Work",
         destination: "London Eye, London, UK",
     },
+    
 ];
-
 
 
 
 const NavFavourites = () => {
     return (
         <FlatList 
-        data={data} 
-        keyExtractor={(item) => item.id}
-        renderItem={({item}) => (
-            <TouchableOpacity>
-                <Text>YO</Text>
+            data={data} 
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={() => (
+                <View style = { [ tw`bg-gray-200`, {height:0.5}] }/>
+            )}
+            renderItem={({item: { location, destination, icon}}) => (
+                <TouchableOpacity style={tw `flex-row items-center p-5`}>
+                    <Icon
+                        style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+                        name={icon}
+                        type="ionicon"
+                        color="white"
+                        size={18}
+                    
+                    
+                    />
 
-            </TouchableOpacity>
+                    <View>
+                        <Text style={tw`font-semibold text-lg`}>{location}</Text>
+                        <Text style={tw`text-gray-500`}>{destination}</Text>
+                    </View>
+
+
+                </TouchableOpacity>
 
         )} 
         />
